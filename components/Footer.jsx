79288e6671fef4f822ce56c824ba178424cefc8b
Mathias Clari Drenik <66589714+MathiasClari@users.noreply.github.com>
2023-02-12 16:00:24 +0100

    Update Footer.jsx
    Update #2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -36,10 +36,10 @@ const Footer = () => (
 
                 <div className="flex items-center justify-between flex-wrap gap-4">
                     <h4 className="font-extrabold text-[24px] text-white">
-                        POSSIEST
+                        VHD
                     </h4>
                     <p className="font-normal text-[14px] text-white opacity-50">
-                        Copyright © 2021 - 2022 Possiest. All rights reserved.
+                        Copyright © 2021 - 2023 VHD. All rights reserved.
                     </p>
 
                     <div className="flex gap-4">
